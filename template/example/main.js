be7fc310798990d153c8a1bc5c400651bb43c8f5
Harhao <huahaoworkspace@163.com>
2020-04-24 19:22:38 +0800

    fix:修改main.js

--- template/example/main.js
+++ template/example/main.js
@@ -1,14 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-{{#if_eq module 'es'}}
-import elSwitch from '../dist/{{name}}.es.js'
-{{/if_eq}}
-{{#if_eq module 'umd'}}
-import elSwitch from '../dist/{{name}}.umd.js'
-{{/if_eq}}
-{{#if_eq module 'cjs'}}
-import elSwitch from '../dist/{{name}}.cjs.js'
-{{/if_eq}}
+import elSwitch from '../dist/{{name}}.{{module}}.js'
 
 import "../dist/{{name}}.min.css"
 
